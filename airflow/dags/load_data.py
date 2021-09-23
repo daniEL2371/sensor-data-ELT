5afd460e17da8653615ddcd782bd0c2881f25793
daaniel zelalem <danielzelalemheru@gmail.com>
2021-09-23 13:38:23 +0000

    add: creating table through a dockerized ariflow

--- airflow/dags/load_data.py
+++ airflow/dags/load_data.py
@@ -2,14 +2,16 @@ from datetime import timedelta
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+
+from airflow.operators.mysql_operator import MySqlOperator
 from airflow.utils.dates import datetime
 from airflow.utils.dates import timedelta
 
 default_args = {
-    'owner': 'astronomer',
+    'owner': '10Academy',
     'depends_on_past': False,
     'start_date': datetime(2021, 9, 23),
-    'email': ['noreply@astronomer.io'],
+    'email': ['danielzelalemheru@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -19,13 +21,21 @@ dag = DAG(
     'data_load_dag',
     default_args=default_args,
     description='An Airflow DAG to invoke simple dbt commands',
-    # schedule_interval=timedelta(days=1),
-    schedule_interval='*/1 * * * *'
+    schedule_interval=timedelta(days=1),
+    # schedule_interval='*/ * * * *'
+)
+
+
+create_table_mysql_task = MySqlOperator(
+    task_id='create_table_mysql',
+    mysql_conn_id='mysql_conn_id',
+    sql=r"""CREATE TABLE  test_table (fname VARCHAR(20), lname VARCHAR(20));""",
+    dag=dag
 )
 
 dbt_run = BashOperator(
     task_id='data_load',
-    bash_command='ls',
+    bash_command='echo helloworld',
     dag=dag
 )
 
--- scripts_airflow/init.sh
+++ scripts_airflow/init.sh
@@ -9,12 +9,16 @@ export AIRFLOW__WEBSERVER__SECRET_KEY
 
 DBT_POSTGRESQL_CONN="postgresql+psycopg2://${DBT_POSTGRES_USER}:${DBT_POSTGRES_PASSWORD}@${DBT_POSTGRES_HOST}:${POSTGRES_PORT}/${DBT_POSTGRES_DB}"
 
+
 cd /dbt && dbt compile
 rm -f /airflow/airflow-webserver.pid
 
 airflow users  create --role Admin --username admin --email danielzelalemheru@gmail.com --firstname admin --lastname admin --password admin
+
 sleep 10
-airflow upgradedb
+airflow db upgrade
 sleep 10
-airflow connections --add --conn_id 'dbt_postgres_instance_raw_data' --conn_uri $DBT_POSTGRESQL_CONN
+
+airflow connections add 'mysql_conn_id'  --conn-uri "mysql+mysqldb://${DBT_MYSQL_USER}:${DBT_MYSQL_PASSWORD}@${DBT_MYSQL_HOST}:${MYSQL_PORT}/${DBT_MYSQL_DB}"
+
 airflow scheduler & airflow webserver
