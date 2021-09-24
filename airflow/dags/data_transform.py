2cffd6f016849ae78e333b77449bd418c6eccfcf
daaniel zelalem <danielzelalemheru@gmail.com>
2021-09-24 12:52:29 +0000

    integrated dbt with airflow

--- airflow/dags/data_transform.py
+++ airflow/dags/data_transform.py
@@ -6,10 +6,10 @@ from airflow.utils.dates import datetime
 from airflow.utils.dates import timedelta
 
 default_args = {
-    'owner': 'astronomer',
+    'owner': '10Academy',
     'depends_on_past': False,
     'start_date': datetime(2021, 9, 23),
-    'email': ['noreply@astronomer.io'],
+    'email': ['danielzelalemheru@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -19,20 +19,26 @@ dag = DAG(
     'dbt_dag',
     default_args=default_args,
     description='An Airflow DAG to invoke simple dbt commands',
-    # schedule_interval=timedelta(days=1),
-    schedule_interval='*/1 * * * *'
+    schedule_interval='@once',
+    # schedule_interval='*/1 * * * *'
+)
+
+dbt_debug = BashOperator(
+    task_id='dbt_debug',
+    bash_command='cd /dbt && dbt debug',
+    dag=dag
 )
 
 dbt_run = BashOperator(
     task_id='dbt_run',
-    bash_command='pwd',
+    bash_command='cd /dbt && dbt run',
     dag=dag
 )
 
 dbt_test = BashOperator(
     task_id='dbt_test',
-    bash_command='ls /var/lib/mysql-files/',
+    bash_command='pwd',
     dag=dag
 )
 
-dbt_run >> dbt_test
+dbt_debug >> dbt_run >> dbt_test
